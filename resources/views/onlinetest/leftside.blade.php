75136d38811c6b799065c9209326a81a67b714f5
Rafael Alexandre Piemontez <piemontez@enova.com.br>
2015-10-21 10:07:20 -0200

    first commit

--- resources/views/onlinetest/leftside.blade.php
+++ resources/views/onlinetest/leftside.blade.php
@@ -11,35 +11,53 @@
       <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
 
       <div class="row">
-        <div class="col-md-4 col-md-offset-2">
-            <h3>Radio</h3>
-            <dl class="dl-horizontal">
-              <dt data-toggle="tooltip" title="endereço">Endereço de acesso</dt>
-              <dd>...</dd>
-              <dt data-toggle="tooltip" title="total">Total de ouvintes</dt>
-              <dd>...</dd>
-            </dl>
-	        <a class="btn btn-primary" href="{{ URL::route('leftsidetest') }}" role="button">Iniciar transmissão &raquo;</a>
-        </div>
-        <div class="col-md-4">
-            <h3>Conexões</h3>
-            <dl class="dl-horizontal">
-              <dt data-toggle="tooltip" title="direta">Direta</dt>
-              <dd>...</dd>
-              <dt data-toggle="tooltip" title="Indireta">Indireta</dt>
-              <dd>...</dd>
-            </dl>
-            <div class="progress">
-              <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                <span class="sr-only">60% Complete</span>
-              </div>
-            </div>
-          </div>
-      </div><!-- /row -->
-      <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h3>Transmissão</h3>
+            <h3>Radio</h3>
+            <div class="row">
+            	<div class="col-md-6">
+	            <dl class="dl-horizontal">
+	              <dt data-toggle="tooltip" title="endereço">Endereço de acesso</dt>
+	              <dd>192.168.10.10:8081</dd>
+	              <dt data-toggle="tooltip" title="total">Total de ouvintes</dt>
+	              <dd>0</dd>
+	            </dl>
+	            </div>
+            	<div class="col-md-6">
+	            <dl class="dl-horizontal">
+	              <dt data-toggle="tooltip" title="endereço">Servidor - bits por segundo</dt>
+	              <dd>0</dd>
+	              <dt data-toggle="tooltip" title="total">Pares - bits por segundo</dt>
+	              <dd>0</dd>
+	            </dl>
+	            </div>
+            </div>
+	        <a class="pull-right btn btn-primary" href="{{ URL::route('leftsidetest') }}" role="button">Iniciar transmissão &raquo;</a>
         </div>
+      </div><!-- /row --><br/>
+
+      <div class="row">
+        <div class="col-md-4 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">bits por segundo</h3>
+          </div>
+          <div class="panel-body">
+            <p>...</p>
+          </div>
+        </div>
+        </div>
+
+        <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">pacotes por segundo</h3>
+          </div>
+          <div class="panel-body">
+            <p>...</p>
+          </div>
+        </div>
+        </div>
+
       </div>
     </div><!-- /container -->
 	@endsection
